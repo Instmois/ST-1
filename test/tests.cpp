793ffc864bf406787cfda4ff8554b8036b67c4b3
Instmois <nikita-moiseev-2003@ya.ru>
2024-03-02 00:52:54 +0300

    1.3

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,16 +4,15 @@
 #include "alg.h"
 
 TEST(Prime, testSmall) {
-    EXPECT_TRUE(checkPrime(1));
+    EXPECT_TRUE(checkPrime(2));
     EXPECT_TRUE(checkPrime(3));
     EXPECT_FALSE(checkPrime(12));
     EXPECT_TRUE(checkPrime(13));
 }
 
 TEST(Prime, tetsBig) {
-    EXPECT_TRUE(checkPrime(1e9 + 4));
+    EXPECT_FALSE(checkPrime(1e9 + 4));
     EXPECT_TRUE(checkPrime(123'456'789));
-    EXPECT_FALSE(checkPrime(1e9 + 2));
 }
 TEST(Prime, tetsVeryBig) {
     EXPECT_TRUE(checkPrime(1e12 + 4));
@@ -21,14 +20,14 @@ TEST(Prime, tetsVeryBig) {
 }
 
 TEST(NPrime, testSmall) {
-    EXPECT_EQ(nPrime(2), 1);
-    EXPECT_EQ(nPrime(3), 2);
-    EXPECT_EQ(nPrime(5), 3);
+    EXPECT_EQ(nPrime(2), 3);
+    EXPECT_EQ(nPrime(3), 5);
+    EXPECT_EQ(nPrime(5), 11);
 }
 
 TEST(NPrime, testBig) {
-    EXPECT_EQ(nPrime(1e6 + 4), 232'331);
-    EXPECT_EQ(nPrime(1'456'789), 212'312);
+    EXPECT_EQ(nPrime(1e6 + 4), 15'485'933);
+    EXPECT_EQ(nPrime(1'456'789), 23'145'179);
 }
 
 TEST(NextPrime, testSmall) {
@@ -38,13 +37,13 @@ TEST(NextPrime, testSmall) {
 }
 
 TEST(NextPrime, testBig) {
-    EXPECT_EQ(nextPrime(1e9 + 4), 1e9 + 9);
-    EXPECT_EQ(nextPrime(123'456'789), 123'456'797);
+    EXPECT_EQ(nextPrime(1e9 + 4), 1'000'000'007);
+    EXPECT_EQ(nextPrime(123'456'789), 123'456'791);
 }
 
 TEST(SumPrime, testSmall) {
     EXPECT_EQ(sumPrime(5), 5);
-    EXPECT_EQ(sumPrime(8), 18);
+    EXPECT_EQ(sumPrime(8), 17);
 }
 TEST(SumPrime, testBig) {
     EXPECT_EQ(sumPrime(10'000), 5'736'396);
@@ -53,5 +52,5 @@ TEST(SumPrime, testBig) {
 TEST(SumPrime, testSum) {
     int sum1 = sumPrime(5);
     int sum2 = sumPrime(8);
-    EXPECT_EQ(sumPrime(sum1+sum2), 23);
+    EXPECT_EQ(sumPrime(sum1+sum2), 77);
 }
